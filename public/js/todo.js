397c6ced758515a00cb665f6905ae7d8b954b856
Eun Joo <eunjoolee787@gmail.com>
2014-12-04 21:32:58 -1000

    add check boxes in each list item

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,11 +3,23 @@ $(function(){
       if(event.which == 13){
         alert('You pressed enter!');
         var value = $("#userInput").val();
-        $(".item-list").append("<li>"+value+"</li>");
-          $(this).val('');
+        var checkbox = $('<input>', {
+          type: 'checkbox'
+        });
+        var list = $('<li>', {
+          html: checkbox
+        });
+        list.append(value);
+        $(".item-list").append(list);
       }
+    });
   });
-});
+
+
+// notes-understand document.ready
+// $(document).ready(function(){
+//   $("#userInput").val("hi jon");
+// });
 
 
 
