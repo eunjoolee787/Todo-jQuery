68c2f3fd2c66268fb3a6828c0702723c61818d63
Eun Joo <eunjoolee787@gmail.com>
2014-12-04 20:41:40 -1000

    set up event listener

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,6 +1,12 @@
 $(function(){
-  
-  console.log($);
+  $("#userInput").keypress(function(event){
+      if(event.which == 13){
+        alert('You pressed enter!');
+        var value = $("#userInput").val();
+        $(".item-list").append("<li>"+value+"</li>");
+          $(this).val('');
+      }
+  });
 });
 
 
