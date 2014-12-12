e24cf22054822dcdd77a6dba0342a4629baf7417
Eun Joo <eunjoolee787@gmail.com>
2014-12-11 18:30:25 -1000

    added comments to js

--- public/js/todo.js
+++ public/js/todo.js
@@ -13,16 +13,16 @@
 $(function(){ //$(document).ready() shortcut
 
 //Add working counter that shows how many items the user has left to do.  
-  function itemsLeft() { // create a function 
-    var count = $(".item").length; //create a count var that finds the length of .item
+  function itemsLeft() { // create a function to contain itemsLeft
+    var count = $(".item").length; //create a count var that gets the length of .item
     return count; // return the count
   }
-  function itemsCompleted() {
-    var count = $(".strike").length;
-    return count;
+  function itemsCompleted() { //create a function to contain itemsCompleted
+    var count = $(".strike").length; //create a count var that gets the length of .strike
+    return count; //return the count
   }
 
-  function updateStatus(count, completed) {
+  function updateStatus(count, completed) { //create a function to contain updateStatus
     $("div#count").text(count + " items in list, " + completed + " items completed"); //create div#count and add text that shows the count in the list
   }
 
@@ -48,10 +48,10 @@ $(function(){ //$(document).ready() shortcut
 
         $(this).val(''); // erase the userInput of the text input field (okay)
         
-        //TO DO-goal is update div#count with text & item in list
-        var count = itemsLeft(); //create a count var to find out the itemsLeft
-        var completed = itemsCompleted();
-        updateStatus(count, completed);
+        //Goal is update div#count with text & item in list
+        var count = itemsLeft(); //create a count var to get the itemsLeft
+        var completed = itemsCompleted(); //create a var to get the itemsCompleted
+        updateStatus(count, completed); //create a updateStatus
         
       } //end of .keypress
   }); //end of function
@@ -66,13 +66,32 @@ $(function(){ //$(document).ready() shortcut
       $(this).siblings("span").first().removeClass("strike");//else return checkbox of span and REMOVE "strike" to the first selection
     }//end of .siblings
     var count = itemsLeft(); //create a count var to find out the itemsLeft
-    var completed = itemsCompleted();
+    var completed = itemsCompleted(); //create a completed var to find the itemsCompleted
     updateStatus(count, completed);  
   });//end of this.checked
 
-  $("#saveButton").on("click", function() {
-    console.log("click");
-  });
+  $("#saveButton").on("click", function() { //create a #saveButton onclick function
+    // console.log("click");
+    var tasksCollection = []; // create a taskCollection array
+
+    $(".item").each(function(index, element) { //create a function that checks each .item of index, element
+        //console.log(index, element);
+        var item = {}; //create a item var to store index & elements
+        // console.log($(element).find("span").text());
+        // console.log($(element).find("span").hasClass("strike"));
+        item.title = $(element).find("span").text(); //create a title to the var item and find the elements, then find the text in the span
+        item.completed = $(element).find("span").hasClass("strike");//create a completed to the var item and find the elements, then find the span and any class of "strike" to see if true or false.
+        // console.log(item);
+        tasksCollection.push(item); //now push(add) item to var tasksCollection.
+    });
+    // console.log(tasksCollection);
+    var data = { //create a var data to store data
+      list_to_save: JSON.stringify(tasksCollection) //stringify the tasksCollection to list_to_save.
+    }
+    $.post("/save", data, function(data) { //post the data 
+
+    });
+  });//end of #saveButton
 
 
   // $("button#save").click(function (e) {
