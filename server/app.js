ad3baad35e5309a66feb9b482966e301ee79f273
Eun Joo <eunjoolee787@gmail.com>
2014-12-11 20:55:35 -1000

    finish level 6

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require("fs");
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -12,19 +13,35 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.post('/save', function (req, res) {
  
-  //console.log('user sent post request');
-  console.log(req.body);
-  // res.send("puppies");
-  res.json({status: "succcess"});
+  console.log('user sent post request');
+  // console.log(req.body);
+  saveToDoList(req.body.list_to_save);
+  res.send("puppies");
+  // res.json({status: "succcess"});
 
   // fs.writeFile("list.json");
+  function saveToDoList(content){
+  // fs = require('fs');
+    fs.writeFile('./public/todo_save.json', content, function (err) {
+      if (err) return console.log(err);
+      console.log('Successfully saved todo, save.json');
+    });
+  }
 });
 
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
+
+
+
+
+
+
+
+
