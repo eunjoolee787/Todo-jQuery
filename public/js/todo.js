5dbd0188a1150e1a95dace89bfe3eb4471e676d8
Eun Joo <eunjoolee787@gmail.com>
2014-12-30 20:31:36 -1000

    Finish Level 1

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,3 +1,46 @@
+/*
+- Add event Listener to INPUT Field x
+- event listener should only add when ENTER key is pressed x
+- get VALUE of INPUT field when enter key is pressed x
+- BUILD NEW LI ITEM with INPUT value x
+- ALL OF ABOVE ONLY HAPPENS WHEN INPUT FIELD IS IN FOCUS
+ */
+
 $(function () {
-  
+
+  $("#userInput").keypress(function(event) {
+    if(event.which == 13) {
+      // console.log("hi");
+      var userInput = $("#userInput").val();
+      var newLiElement = $("<li>", {
+        class: "input",
+        text: userInput
+      });
+      $("#tasks").append(newLiElement);
+      $(this).val("");
+    }
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
