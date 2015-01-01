92e5d0eb1ba9f7fd83980b69e653f6bc1ffc6ba5
Eun Joo <eunjoolee787@gmail.com>
2014-12-31 17:55:49 -1000

    revised level 1

--- public/js/todo.js
+++ public/js/todo.js
@@ -10,18 +10,44 @@ $(function () {
 
   $("#userInput").keypress(function(event) {
     if(event.which == 13) {
-      // console.log("hi");
       var userInput = $("#userInput").val();
-      var newLiElement = $("<li>", {
-        class: "input",
-        text: userInput
-      });
-      $("#tasks").append(newLiElement);
+      var newLiElement = $("<li>");
+      
+      newLiElement.addClass("todos");
+      newLiElement.text("  " + userInput);
+
+      var newCheckBox = $("<input>");
+      newCheckBox.attr('type', 'checkbox');
+      newCheckBox.addClass("checkBox");
+
+      newLiElement.prepend(newCheckBox);
+
+      $("#tasksList").append(newLiElement);
       $(this).val("");
     }
+    updateCounter();
   });
-});
 
+  $(".checkBox").click(function(event) {
+    if(this.checked) {
+      $(this).parent().addClass("strike");
+    } else {
+      $(this).parent().removeClass("strike");
+    }
+    updateCounter();
+  });
+
+  function updateCounter() {
+    var itemTotal = $(".todos").length;
+    var itemCompleted = $(".strike").length;
+    var itemLeft = itemTotal - itemCompleted;
+
+    $(".itemLeft").text(itemLeft);
+    $(".itemCompleted").text(itemCompleted);
+  }
+  updateCounter();
+
+});//end of doc ready
 
 
 
